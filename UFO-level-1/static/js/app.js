8c44a958fbbccac8c451348f8607208f39e106dc
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-09-13 19:18:40 +0800

    Add table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
-// from data.js
+// Fetch sighting data from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Add table to web page and append rows for each sighting
+
+// Select table
+var tbody = d3.select("tbody");
+
+// Add rows and data
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// Event listener for date entered in form
+
+// Filter sighting data by datetime
