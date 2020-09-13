477f4e2c7020384afb7539678426e5aae22a9522
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-09-13 20:42:45 +0800

    Add filter function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -16,34 +16,46 @@ data.forEach((sighting) => {
   });
 
 
-// Event listener for form
-
 // Select the button
 var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select(".filter");
+var form = d3.select("#form");
 
-// Create event handlers for clicking the button or pressing the enter key
+// Create event handlers for clicking the filter button or pressing the enter key
 button.on("click", runFilter);
 form.on("submit",runFilter);
 
-// Create the function to run for both events
+// Create the function to run for the form
 function runFilter() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-  // Print the value to the console
-  console.log(inputValue);
+    // Print the value to the console
+    console.log(inputValue);
 
+    // Clear the unfiltered data
+    tbody.html("");
+
+    // Filter table based on input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log(filteredData);
+
+    // Display the filtered dataset
+    filteredData.forEach((sighting) => {
+    var row = tbody.append('tr');
+    Object.entries(sighting).forEach(([key, value]) => {
+        // console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+        });
+    });
 };
 
-
-// Filter sighting data by datetime 
